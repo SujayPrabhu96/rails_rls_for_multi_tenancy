3941f5037941e23a8865fc724c3d83e055fbec57
SujayPrabhu96 <prabhusujay@gmail.com>
2024-08-01 12:31:35 +0530

    Set the current tenant from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :init_row_level_security
+
+  def init_row_level_security
+    # Get the warehouse information from the request or the session
+    # For simplicity, I am using the first warehouse as the default or get from request headers
+    current_tenant = Warehouse.find_by(id: request.headers['X-Tenant'] || 1)
+
+    if current_tenant
+      RLS.set_tenant current_tenant
+    else
+      puts "Tenant not found"
+    end
+  end
 end
